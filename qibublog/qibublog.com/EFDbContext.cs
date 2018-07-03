237e7f7893290fad01b1f09c34ef8f663165e80a
vic_lnh <vic_leh@meten.com>
2018-07-03 23:42:46 +0800

    调整规范项目

---
+++ qibublog/qibublog.com/EFDbContext.cs
@@ -0,0 +1,20 @@
+﻿using qibublog.model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QiBuBlog.Com
+{
+    public class EFDbContext<TEntity>
+    {
+        public DbContext Instance = new Entities();
+
+        public EFDbContext()
+        {
+            //
+        }
+    }
+}
