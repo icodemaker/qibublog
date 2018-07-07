a5d011aab725d03c49a4e9e6d2e45b1fdaee9d25
vic_lnh <vic_leh@meten.com>
2018-07-07 23:53:36 +0800

    Git Config Already

---
+++ Code/QiBuBlog.UnitTest/ArticleUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QiBuBlog.UnitTest
+{
+    [TestClass]
+    public class ArticleUnitTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+
+        }
+
+        [TestMethod]
+        public void PageList()
+        {
+
+        }
+
+        [TestMethod]
+        public void Del()
+        {
+
+        }
+    }
+}
---
+++ Code/QiBuBlog.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QiBuBlog.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QiBuBlog.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c09c095a-1aee-431b-8e59-e31caf015cad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
