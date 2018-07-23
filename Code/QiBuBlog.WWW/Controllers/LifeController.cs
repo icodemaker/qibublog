b5b6be67fc0e668f98e89971ac0ffbda45c9c203
vic_lnh <vic_leh@meten.com>
2018-07-23 20:06:16 +0800

    考虑设计三个自定义页面

---
+++ Code/QiBuBlog.WWW/Controllers/LifeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QiBuBlog.WWW.Controllers
+{
+    public class LifeController : BaseController
+    {
+        //
+        // GET: /Life/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Code/QiBuBlog.WWW/Controllers/TimelineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QiBuBlog.WWW.Controllers
+{
+    public class TimelineController : BaseController
+    {
+        //
+        // GET: /Timeline/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
