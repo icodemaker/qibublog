0514eb126bae543bd3ba92998f0471dcd276a594
eh <eh@qibu.net.cn>
2018-09-25 02:32:53 +0800

    解决方案和编辑器文件上传冲突

--- Code/QiBuBlog.WWW/Scripts/ueditor/third-party/highcharts/modules/no-data-to-display.js
+++ Code/QiBuBlog.WWW/Scripts/ueditor/third-party/highcharts/modules/no-data-to-display.js
@@ -1,12 +1,1 @@
-/*
- Highcharts JS v3.0.6 (2013-10-04)
- Plugin for displaying a message when there is no data visible in chart.
-
- (c) 2010-2013 Highsoft AS
- Author: Øystein Moseng
-
- License: www.highcharts.com/license
-*/
-(function(c){function f(){return!!this.points.length}function g(){this.hasData()?this.hideNoData():this.showNoData()}var d=c.seriesTypes,e=c.Chart.prototype,h=c.getOptions(),i=c.extend;i(h.lang,{noData:"No data to display"});h.noData={position:{x:0,y:0,align:"center",verticalAlign:"middle"},attr:{},style:{fontWeight:"bold",fontSize:"12px",color:"#60606a"}};d.pie.prototype.hasData=f;if(d.gauge)d.gauge.prototype.hasData=f;if(d.waterfall)d.waterfall.prototype.hasData=f;c.Series.prototype.hasData=function(){return this.dataMax!==
-void 0&&this.dataMin!==void 0};e.showNoData=function(a){var b=this.options,a=a||b.lang.noData,b=b.noData;if(!this.noDataLabel)this.noDataLabel=this.renderer.label(a,0,0,null,null,null,null,null,"no-data").attr(b.attr).css(b.style).add(),this.noDataLabel.align(i(this.noDataLabel.getBBox(),b.position),!1,"plotBox")};e.hideNoData=function(){if(this.noDataLabel)this.noDataLabel=this.noDataLabel.destroy()};e.hasData=function(){for(var a=this.series,b=a.length;b--;)if(a[b].hasData()&&!a[b].options.isInternal)return!0;
-return!1};e.callbacks.push(function(a){c.addEvent(a,"load",g);c.addEvent(a,"redraw",g)})})(Highcharts);
+﻿(function(a){function k(){return !!this.points.length}function l(){this.hasData()?this.hideNoData():this.showNoData()}var b=a.seriesTypes,j=a.Chart.prototype,m=a.getOptions(),n=a.extend;n(m.lang,{noData:"No data to display"});m.noData={position:{x:0,y:0,align:"center",verticalAlign:"middle"},attr:{},style:{fontWeight:"bold",fontSize:"12px",color:"#60606a"}};b.pie.prototype.hasData=k;if(b.gauge){b.gauge.prototype.hasData=k}if(b.waterfall){b.waterfall.prototype.hasData=k}a.Series.prototype.hasData=function(){return this.dataMax!==void 0&&this.dataMin!==void 0};j.showNoData=function(c){var d=this.options,c=c||d.lang.noData,d=d.noData;if(!this.noDataLabel){this.noDataLabel=this.renderer.label(c,0,0,null,null,null,null,null,"no-data").attr(d.attr).css(d.style).add(),this.noDataLabel.align(n(this.noDataLabel.getBBox(),d.position),!1,"plotBox")}};j.hideNoData=function(){if(this.noDataLabel){this.noDataLabel=this.noDataLabel.destroy()}};j.hasData=function(){for(var c=this.series,d=c.length;d--;){if(c[d].hasData()&&!c[d].options.isInternal){return !0}}return !1};j.callbacks.push(function(c){a.addEvent(c,"load",l);a.addEvent(c,"redraw",l)})})(Highcharts);
